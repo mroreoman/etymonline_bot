20987c9a87374b3c7d9744ab2e760a65c499602e
aryan singh <aryan.singh@motsd.org>
2024-11-05 12:26:41 -0500

    moved search into a function

--- main.py
+++ main.py
@@ -2,21 +2,24 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 
-if len(sys.argv) == 1:
-    print("no word given. using bye\n")
-    sys.argv.append("bye")
+def search(word):
+    page = requests.get("https://www.etymonline.com/word/" + word)
+    soup = BeautifulSoup(page.content, "html.parser")
+    entries = soup.find_all("div", class_="word--C9UPa")
+    if not entries:
+        print("could not find " + word + " on etymonline.com")
+    else:
+        print("etymonline.com results for " + word + "\n")
+        for entry in entries:
+            name = entry.find(["h1", "p"], class_="word__name--TTbAA") # ignores related words
+            if name:
+                print(name.get_text())
+                definition = name.find_next_sibling("section", class_="word__defination--2q7ZH")
+                print(definition.get_text() + "\n") # formatting is lost (blackquote, italics, links)
 
-word = sys.argv[1].strip()
-page = requests.get("https://www.etymonline.com/word/" + word)
-soup = BeautifulSoup(page.content, "html.parser")
-entries = soup.find_all("div", class_="word--C9UPa")
-if not entries:
-    print("could not find " + word + " on etymonline.com")
-else:
-    print("etymonline.com results for " + word + "\n")
-    for entry in entries:
-        name = entry.find(["h1", "p"], class_="word__name--TTbAA") # ignores related words
-        if name:
-            print(name.get_text())
-            definition = name.find_next_sibling("section", class_="word__defination--2q7ZH")
-            print(definition.get_text() + "\n") # formatting is lost (blackquote, italics, links)
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        print("no word given. using bye\n")
+        sys.argv.append("bye")
+    search(sys.argv[1])
